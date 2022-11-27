1905fb78759682fc9e6b2e3142eb3c09689f7dd1
szantaii <szantaii@gmail.com>
2022-11-27 22:16:41 +0100

    update-screen: Add support to display custom 4bpp image file format
    The image format is the following:
    
        +-------------------------------------------------------------+
        | 2 bytes encoding the image width in pixels (little endian)  |
        +-------------------------------------------------------------+
        | 2 bytes encoding the image height in pixels (little endian) |
        +-------------------------------------------------------------+
        | image data: Each following byte encodes two pixels of the   |
        |             image. The upper four bits encode the first     |
        |             pixel, the lower four bits the next pixel and   |
        |             so on. Pixels are encoded from top to bottom    |
        |             row by row, from right to left.                 |
        +-------------------------------------------------------------+
    
        0        1        2        3        4        (width * height) / 2 + 3
        +--------+--------+--------+--------+- . . . +--------+
        |        |        |        |        |        |        |
        +--------+--------+--------+--------+- . . . +--------+
        :                 :                 :
        :   image width   :  image height   :
        :     2 bytes     :     2 bytes     :
        :  little endian  :  little endian  :
        :                 :                 :
    
    Notes:
        * It is not required to have any image data at all.
        * The minimum file size is 4 bytes.
        * The maximum image resolution is 2^16 - 2 pixels by 2^16 - 1 pixels
            or vice versa (resulting in a 2,147,385,349 bytes of maximum file size).
        * The number of pixels in the image must be even: (width * height) mod 2 == 0.
        * The last index of the last byte (the last two pixels if image has data)
            is (width * height) / 2 + 3.

--- src/update-screen/main.c
+++ src/update-screen/main.c
@@ -7,17 +7,25 @@
 #include <math.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <endian.h>
+#include <string.h>
 
 void print_help(void);
+int str_ends_with(const char *str, const char *substr);
 int display_bmp_image(
+    IT8951_Dev_Info device_info,
+    uint32_t target_memory_address,
+    const char *file_path);
+int display_4bpp_image(
     IT8951_Dev_Info device_info,
     uint32_t target_memory_address,
     const char *file_path);
 
 int main(int argc, char *argv[])
 {
+    int exit_status = 0;
     int opt = 0;
-    bool display_image = false;
     char *file_path = NULL;
     double tmp_vcom = 0.0;
     uint16_t vcom = 0;
@@ -57,8 +65,6 @@ int main(int argc, char *argv[])
 
                 return 1;
             }
-
-            display_image = true;
 
             break;
 
@@ -113,29 +119,56 @@ int main(int argc, char *argv[])
 
     if (strcmp((char *)device_info.LUT_Version, "M841_TFA5210") != 0)
     {
+        exit_status = 3;
+
         fprintf(
             stderr,
             "%s\n",
             "Connected device is not a 10.3 inch e-paper device.");
-
-        return 3;
     }
 
     target_memory_address = device_info.Memory_Addr_L | (device_info.Memory_Addr_H << 16);
 
-    if (display_image)
+    if (file_path != NULL)
     {
-        if (display_bmp_image(device_info, target_memory_address, file_path) != 0)
+        if (str_ends_with(file_path, ".bmp") == 0)
         {
+            if (display_bmp_image(device_info, target_memory_address, file_path) != 0)
+            {
+                exit_status = 4;
+
+                fprintf(
+                    stderr,
+                    "%s (%s).\n",
+                    "Error during drawing BMP image onto screen",
+                    file_path);
+
+                EPD_IT8951_Clear_Refresh(device_info, target_memory_address, INIT_Mode);
+            }
+        }
+        else if (str_ends_with(file_path, ".4bpp") == 0)
+        {
+            if (display_4bpp_image(device_info, target_memory_address, file_path) != 0)
+            {
+                exit_status = 4;
+
+                fprintf(
+                    stderr,
+                    "%s (%s).\n",
+                    "Error during drawing 4bpp image onto screen",
+                    file_path);
+
+                EPD_IT8951_Clear_Refresh(device_info, target_memory_address, INIT_Mode);
+            }
+        }
+        else
+        {
+            exit_status = 5;
+
             fprintf(
                 stderr,
-                "%s (%s).\n",
-                "Error during drawing BMP image onto screen",
-                file_path);
-
-            EPD_IT8951_Clear_Refresh(device_info, target_memory_address, INIT_Mode);
-
-            return 4;
+                "%s\n",
+                "Unsupported image file format.");
         }
     }
     else
@@ -146,7 +179,7 @@ int main(int argc, char *argv[])
     EPD_IT8951_Sleep();
     DEV_Module_Exit();
 
-    return 0;
+    return exit_status;
 }
 
 void print_help(void)
@@ -156,10 +189,6 @@ void print_help(void)
 
         "%s\n"
         "%s\n"
-        "%s\n\n"
-
-        "%s\n"
-        "%s\n"
         "%s\n"
         "%s\n\n"
 
@@ -171,13 +200,26 @@ void print_help(void)
         "%s\n"
         "%s\n"
         "%s\n"
-        "%s\n"
-        "%s\n"
-        "%s\n",
+        "%s\n\n"
 
-        "Usage: update-screen -v VOLTAGE [-f <BMP_FILE> | -h]",
+        "%s\n"
+        "%s\n"
+        "%s\n"
+        "%s\n"
+        "%s\n"
+        "%s\n"
+        "%s\n\n"
+
+        "%s\n"
+        "%s\n"
+        "%s\n"
+        "%s\n"
+        "%s\n\n",
+
+        "Usage: update-screen -v VOLTAGE [-f <IMAGE_FILE> | -h]",
         "Update the screen of the connected 10.3 e-paper device either",
-        "by clearing it or drawing a 8bit/channel RGB BMP image on it.",
+        "by clearing it or drawing a 8bit per channel RGB BMP or a custom",
+        "4bits per pixel image on it.",
 
         "Mandatory option:",
         "  -v VOLTAGE  use the VOLTAGE for the connected e-paper device",
@@ -185,16 +227,42 @@ void print_help(void)
         "              printed circuit cable of the connected device.)",
 
         "Optional options:",
-        "  -f BMP_FILE  draw the 8bit/channel RGB BMP_FILE on the screen of",
-        "               the connected e-paper device",
-        "  -h           display this help and exit",
+        "  -f IMAGE_FILE  draw the specified image on the screen of the",
+        "                 connected e-paper device",
+        "  -h             display this help and exit",
 
         "Exit status:",
         "  0  success",
         "  1  wrong command-line arguments or command-line parsing error",
         "  2  failed to initialize bcm2835 device",
         "  3  the connected device is not a 10.3 inch e-paper device",
-        "  4  error during drawing BMP image onto screen");
+        "  4  error during drawing BMP/4BPP image onto screen",
+        "  5  unsupported image file format",
+
+        "Examples:",
+        "  ./update-screen -h",
+        "  ./update-screen -v -2.51",
+        "  ./update-screen -v -1.50 -f /path/to/image.bmp",
+        "  ./update-screen -v -1.48 -f /path/to/image.4bpp");
+}
+
+int str_ends_with(const char *str, const char *substr)
+{
+    if (str == NULL || substr == NULL)
+    {
+        return -1;
+    }
+
+    size_t str_length = strlen(str);
+    size_t substr_length = strlen(substr);
+
+    if (str_length >= substr_length
+        && strcmp(str + (str_length - substr_length), substr) == 0)
+    {
+        return 0;
+    }
+
+    return 1;
 }
 
 int display_bmp_image(
@@ -262,6 +330,181 @@ int display_bmp_image(
     {
         free(image);
         image = NULL;
+    }
+
+    return 0;
+}
+
+int display_4bpp_image(
+    IT8951_Dev_Info device_info,
+    uint32_t target_memory_address,
+    const char *file_path)
+{
+    struct stat file_status;
+    size_t file_size = 0;
+    FILE *fp = NULL;
+    uint16_t image_width = 0;
+    uint16_t image_height = 0;
+    size_t image_data_size = 0;
+    uint8_t *image_data = NULL;
+    size_t image_header_size = sizeof(image_width) + sizeof(image_height);
+    uint16_t maximum_resolution = (uint16_t)(pow(2.0, 16.0) - 1.0);
+
+    if (stat(file_path, &file_status) != 0)
+    {
+        fprintf(
+            stderr,
+            "%s (%s).\n",
+            "Unable to get file status",
+            file_path);
+
+        return -1;
+    }
+
+    file_size = (size_t)file_status.st_size;
+
+    if (file_size < image_header_size)
+    {
+        fprintf(
+            stderr,
+            "%s (%s).\n",
+            "File size is smaller than the minimum size of a 4bpp image",
+            file_path);
+
+        return -2;
+    }
+
+    if (file_size == image_header_size)
+    {
+        EPD_IT8951_Clear_Refresh(device_info, target_memory_address, INIT_Mode);
+
+        fprintf(
+            stderr,
+            "%s (%s).\n",
+            "No image data in file, not drawing anything to screen",
+            file_path);
+
+        return 0;
+    }
+
+    fp = fopen(file_path, "rb");
+
+    if (fp == NULL)
+    {
+        fprintf(
+            stderr,
+            "%s (%s).\n",
+            "Cannot open file",
+            file_path);
+
+        return -3;
+    }
+
+    if (!(fread(&image_width, sizeof(image_width), 1, fp) == 1
+        && fread(&image_height, sizeof(image_height), 1, fp) == 1))
+    {
+        fclose(fp);
+
+        fprintf(
+            stderr,
+            "%s (%s).\n",
+            "Invalid 4bpp image file",
+            file_path);
+
+        return -4;
+    }
+
+    image_width = le16toh(image_width);
+    image_height = le16toh(image_height);
+
+    if (!((image_width <= maximum_resolution && image_height < maximum_resolution)
+        || (image_width < maximum_resolution && image_height <= maximum_resolution)))
+    {
+        fclose(fp);
+
+        fprintf(
+            stderr,
+            "%s\n",
+            "Image resolution is higher than the maximum allowed "
+            "(2^16 - 2 pixels by 2^16 - 1 pixels).");
+
+        return -5;
+    }
+
+    if (((image_width * image_height) % 2) != 0)
+    {
+        fclose(fp);
+
+        fprintf(
+            stderr,
+            "%s\n",
+            "The number of pixels in this 4bpp image is odd! "
+            "The number of pixels in a 4bpp image must be even.");
+
+        return -6;
+    }
+
+    image_data_size = (image_width * image_height) / 2;
+
+    if (file_size != image_header_size + image_data_size)
+    {
+        fclose(fp);
+
+        fprintf(
+            stderr,
+            "%s (%s).\n",
+            "Invalid 4bpp image file",
+            file_path);
+
+        return -7;
+    }
+
+    image_data = malloc(image_data_size);
+
+    if (image_data == NULL)
+    {
+        fclose(fp);
+
+        fprintf(
+            stderr,
+            "%s\n",
+            "Cannot allocate enough memory for 4bpp image data.");
+
+        return -8;
+    }
+
+    if (fread(image_data, image_data_size, 1, fp) != 1)
+    {
+        free(image_data);
+        image_data = NULL;
+
+        fclose(fp);
+
+        fprintf(
+            stderr,
+            "%s\n",
+            "Failed to read 4bpp image data.");
+
+        return -9;
+    }
+
+    fclose(fp);
+
+    EPD_IT8951_Clear_Refresh(device_info, target_memory_address, INIT_Mode);
+    EPD_IT8951_4bp_Refresh(
+        image_data,
+        0,
+        0,
+        device_info.Panel_W,
+        device_info.Panel_H,
+        true,
+        target_memory_address,
+        false);
+
+    if (image_data != NULL)
+    {
+        free(image_data);
+        image_data = NULL;
     }
 
     return 0;
