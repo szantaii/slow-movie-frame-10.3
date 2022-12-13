132d481bc1d0e71184e0b4113a82db9ed229b466
szantaii <szantaii@gmail.com>
2022-12-13 20:08:38 +0100

    slow-movie-player-service: Add check for mandatory config settings

--- src/slow-movie-player-service/configuration.py
+++ src/slow-movie-player-service/configuration.py
@@ -1,15 +1,28 @@
 from skip import FrameSkip, TimeSkip
 from grayscalemethod import GrayscaleMethod
 
+from typing import Union
 import configparser
 import re
 import os
+import math
 
 
 class Configuration:
     SECTION_NAME = 'slow_movie_player'
 
     def __init__(self, config_directory: str) -> None:
+        # Mandatory config entries
+        self.vcom : float = float('-inf')
+        self.screen_width : int = 0
+        self.screen_height : int  = 0
+        self.refresh_timeout : float = 0.0
+        self.video_directory : str = ''
+        # Optional config entries
+        self.skip : Union[FrameSkip, TimeSkip] = FrameSkip(1)
+        self.grayscale_method : GrayscaleMethod = GrayscaleMethod('')
+
+        # Read config
         config_path = self.__get_first_config_file_path_from_directory(config_directory)
 
         with open(config_path, 'r') as config_file:
@@ -23,12 +36,14 @@ class Configuration:
         parser.empty_lines_in_values = False
         parser.read_string(config_string)
 
+        # VCOM (mandatory)
         self.vcom = parser.getfloat(self.__class__.SECTION_NAME, 'vcom')
 
         display_resolution_str = self.__strip_enclosing_quotes(
             parser.get(self.__class__.SECTION_NAME, 'display_resolution')
         )
 
+        # Display resolution (mandatory)
         match = re.fullmatch(r'(\d+)\s*[xX,;\s]\s*(\d+)', display_resolution_str)
 
         if not match:
@@ -37,9 +52,15 @@ class Configuration:
         self.screen_width = int(match[1])
         self.screen_height = int(match[2])
 
+        # Refresh timeout (mandatory)
         self.refresh_timeout = parser.getfloat(self.__class__.SECTION_NAME, 'refresh_timeout')
 
-        self.skip = FrameSkip(1)
+        # Video directory (mandatory)
+        self.video_directory = self.__strip_enclosing_quotes(
+            parser.get(self.__class__.SECTION_NAME, 'video_directory')
+        )
+
+        # Time/FrameSkip (optional)
         frame_skip = parser.getint(self.__class__.SECTION_NAME, 'frame_skip', fallback=None)
         time_skip = parser.getfloat(self.__class__.SECTION_NAME, 'time_skip', fallback=None)
 
@@ -48,15 +69,19 @@ class Configuration:
         elif not time_skip and frame_skip:
             self.skip = FrameSkip(frame_skip)
 
+        # Grayscale method (optional)
         self.grayscale_method = GrayscaleMethod(
             self.__strip_enclosing_quotes(
                 parser.get(self.__class__.SECTION_NAME, 'grayscale_method')
             )
         )
 
-        self.video_directory = self.__strip_enclosing_quotes(
-            parser.get(self.__class__.SECTION_NAME, 'video_directory')
-        )
+        if (math.isinf(self.vcom)
+                or self.screen_width <= 0
+                or self.screen_height <= 0
+                or self.refresh_timeout < 0.0
+                or not self.video_directory):
+            raise RuntimeError('Invalid configuration')
 
     @staticmethod
     def __get_first_config_file_path_from_directory(config_directory: str) -> str:
