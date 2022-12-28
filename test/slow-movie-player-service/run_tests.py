049090d4fce4cb728058d6737c03f53a47e412ea
szantaii <szantaii@gmail.com>
2022-12-28 17:28:52 +0100

    slow-movie-player-service: Add tests for Display

--- test/slow-movie-player-service/run_tests.py
+++ test/slow-movie-player-service/run_tests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import display_test
 import grayscalemethod_test
 import skip_test
 import video_test
@@ -8,6 +9,7 @@ import unittest
 if __name__ == '__main__':
     test_suite = unittest.TestSuite()
     test_modules = [
+        display_test,
         grayscalemethod_test,
         skip_test,
         video_test,
