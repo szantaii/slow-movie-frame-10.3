b97a8a5b676b77a6f33ce59721141bd152f76670
szantaii <szantaii@gmail.com>
2023-01-02 16:27:35 +0100

    slow-movie-player-service: Add fixes to VideoLibrary
     - Rename variables to better reflect their purpose
     - Add check for non existing video directory at instantiation
     - Add check for empty video library when getting the next frame
     - Return fast if there is no video library file (yet) when trying to load it from the filesystem
     - Add more meaningful exception messages (e.g. in case of JSONDecodingError, and the "we are fucked" message was not particularly helpful either)

--- src/slow-movie-player-service/videolibrary.py
+++ src/slow-movie-player-service/videolibrary.py
@@ -9,7 +9,7 @@ import os
 
 
 class VideoLibrary:
-    PLAYBACK_INFO_FILE_NAME = 'videos.json'
+    VIDEO_LIBRARY_FILE_NAME = 'videos.json'
     BACKUP_FILE_EXTENSION = '.bak'
     TEMPORARY_FILE_EXTENSION = '.tmp'
     VIDEO_FILE_EXTENSIONS = (
@@ -21,18 +21,24 @@ class VideoLibrary:
     )
 
     def __init__(self, video_directory: str) -> None:
-        self.__playback_info = OrderedDict()
-        self.__video_directory = video_directory
+        if not os.path.exists(video_directory):
+            raise FileNotFoundError("Video directory '{}' does not exist.".format(video_directory))
 
-        self.__playback_info_file_path = os.path.join(self.__video_directory, self.__class__.PLAYBACK_INFO_FILE_NAME)
+        self.__video_library: OrderedDict = OrderedDict()
+        self.__video_directory: str = video_directory
 
-        self.__temp_playback_info_file_path = '{}{}'.format(
-            self.__playback_info_file_path,
+        self.__video_library_file_path: str = os.path.join(
+            self.__video_directory,
+            self.__class__.VIDEO_LIBRARY_FILE_NAME
+        )
+
+        self.__temp_video_library_file_path: str = '{}{}'.format(
+            self.__video_library_file_path,
             self.__class__.TEMPORARY_FILE_EXTENSION
         )
 
-        self.__backup_playback_info_file_path = '{}{}'.format(
-            self.__playback_info_file_path,
+        self.__backup_video_library_file_path: str = '{}{}'.format(
+            self.__video_library_file_path,
             self.__class__.BACKUP_FILE_EXTENSION
         )
 
@@ -41,66 +47,84 @@ class VideoLibrary:
         self.__save_to_file()
 
     def __load_from_file(self) -> None:
-        try:
-            with open(self.__playback_info_file_path) as playback_info_file:
-                playback_info_file_contents = playback_info_file.read()
+        if not os.path.exists(self.__video_library_file_path):
+            return
 
-                try:
-                    self.__playback_info = json.loads(playback_info_file_contents, object_pairs_hook=OrderedDict)
-                except json.JSONDecodeError as error:
-                    print(
-                        "Caught exception: '{}'!\n"
-                        "Cannot load playback information from '{}'.".format(error.msg, self.__playback_info_file_path)
-                    )
-        except FileNotFoundError:
-            print(
-                "File '{}' does not exist! "
-                'Cannot load playback information.'.format(self.__playback_info_file_path)
+        with open(self.__video_library_file_path) as video_library_file:
+            video_library_file_contents = video_library_file.read()
+
+        try:
+            self.__video_library = json.loads(video_library_file_contents, object_pairs_hook=OrderedDict)
+        except json.JSONDecodeError as error:
+            raise ValueError(
+                (
+                    "Cannot load video library from '{}'. "
+                    "It is recommended to check for backup ('{}') and temporary ('{}') files in '{}' "
+                    'for manually recovering video library data. '
+                    "See details for easier debugging: '{}'."
+                ).format(
+                    self.__video_library_file_path,
+                    self.__class__.BACKUP_FILE_EXTENSION,
+                    self.__class__.TEMPORARY_FILE_EXTENSION,
+                    self.__video_directory,
+                    error.__repr__()
+                )
             )
 
-    def __compare_playback_info_to_file(self, file_path: str) -> None:
-        playback_info_string = json.dumps(self.__playback_info, ensure_ascii=False, indent=4)
+    def __compare_video_library_to_file(self, file_path: str) -> None:
+        video_library_string = json.dumps(self.__video_library, ensure_ascii=False, indent=4)
 
-        with open(file_path, 'r') as playback_info_file:
-            playback_info_file_contents = playback_info_file.read()
+        with open(file_path, 'r') as video_library_file:
+            video_library_file_contents = video_library_file.read()
 
-        if playback_info_string != playback_info_file_contents:
-            raise ValueError('we are fucked')
+        if video_library_string != video_library_file_contents:
+            raise ValueError(
+                (
+                    "Cannot save video library to '{}'. "
+                    "It is recommended to check for backup ('{}') and temporary ('{}') files in '{}' "
+                    'for manually recovering video library data.'
+                ).format(
+                    self.__video_library_file_path,
+                    self.__class__.BACKUP_FILE_EXTENSION,
+                    self.__class__.TEMPORARY_FILE_EXTENSION,
+                    self.__video_directory
+                )
+            )
 
     def __save_to_file(self) -> None:
-        temp_playback_info_file = os.open(
-            self.__temp_playback_info_file_path,
+        temp_video_library_file = os.open(
+            self.__temp_video_library_file_path,
             os.O_CREAT | os.O_WRONLY
         )
 
         os.write(
-            temp_playback_info_file,
-            json.dumps(self.__playback_info, ensure_ascii=False, indent=4).encode('utf-8')
+            temp_video_library_file,
+            json.dumps(self.__video_library, ensure_ascii=False, indent=4).encode('utf-8')
         )
-        os.fsync(temp_playback_info_file)
-        os.close(temp_playback_info_file)
+        os.fsync(temp_video_library_file)
+        os.close(temp_video_library_file)
 
-        self.__compare_playback_info_to_file(self.__temp_playback_info_file_path)
+        self.__compare_video_library_to_file(self.__temp_video_library_file_path)
 
-        if os.access(self.__playback_info_file_path, os.F_OK, follow_symlinks=False):
-            os.rename(self.__playback_info_file_path, self.__backup_playback_info_file_path)
+        if os.access(self.__video_library_file_path, os.F_OK, follow_symlinks=False):
+            os.rename(self.__video_library_file_path, self.__backup_video_library_file_path)
 
-        os.rename(self.__temp_playback_info_file_path, self.__playback_info_file_path)
+        os.rename(self.__temp_video_library_file_path, self.__video_library_file_path)
 
-        self.__compare_playback_info_to_file(self.__playback_info_file_path)
+        self.__compare_video_library_to_file(self.__video_library_file_path)
 
-        if os.access(self.__backup_playback_info_file_path, os.F_OK, follow_symlinks=False):
-            os.unlink(self.__backup_playback_info_file_path)
+        if os.access(self.__backup_video_library_file_path, os.F_OK, follow_symlinks=False):
+            os.unlink(self.__backup_video_library_file_path)
 
     def __reset(self) -> None:
-        for _, info in self.__playback_info.items():
-            info['next_frame'] = 0
-            info['next_timestamp'] = 0.0
+        for _, video_info in self.__video_library.items():
+            video_info['next_frame'] = 0
+            video_info['next_timestamp'] = 0.0
 
         self.__save_to_file()
 
     def __discover(self) -> None:
-        video_paths = list(self.__playback_info.keys())
+        video_paths = list(self.__video_library.keys())
 
         for root_directory, _, files in os.walk(self.__video_directory):
             for file in files:
@@ -116,10 +140,10 @@ class VideoLibrary:
         video_paths = sorted(video_paths)
 
         for video_path in video_paths:
-            if video_path not in self.__playback_info.keys():
+            if video_path not in self.__video_library.keys():
                 video = Video(video_path)
                 frame_count, duration = video.get_stats()
-                self.__playback_info[video_path] = OrderedDict(
+                self.__video_library[video_path] = OrderedDict(
                     {
                         'frame_count': frame_count,
                         'duration': duration,
@@ -131,21 +155,24 @@ class VideoLibrary:
                 continue
 
             if not os.path.exists(video_path):
-                del self.__playback_info[video_path]
+                del self.__video_library[video_path]
 
                 continue
 
             video = Video(video_path)
             frame_count, duration = video.get_stats()
 
-            if (self.__playback_info[video_path]['frame_count'] != frame_count
-                    or self.__playback_info[video_path]['duration'] != duration):
-                self.__playback_info[video_path]['frame_count'] = frame_count
-                self.__playback_info[video_path]['duration'] = duration
-                self.__playback_info[video_path]['next_frame'] = 0
-                self.__playback_info[video_path]['next_timestamp'] = 0.0
+            if (self.__video_library[video_path]['frame_count'] != frame_count
+                    or self.__video_library[video_path]['duration'] != duration):
+                self.__video_library[video_path]['frame_count'] = frame_count
+                self.__video_library[video_path]['duration'] = duration
+                self.__video_library[video_path]['next_frame'] = 0
+                self.__video_library[video_path]['next_timestamp'] = 0.0
 
     def get_next_frame(self, skip: Union[FrameSkip, TimeSkip] = FrameSkip(1)) -> numpy.ndarray:
+        if not self.__video_library:
+            raise RuntimeError('Cannot get next frame from empty video library!')
+
         if not isinstance(skip, (FrameSkip, TimeSkip)):
             raise TypeError(
                 "Argument 'skip' is of type '{}', but should be of type '{}' or '{}' instead.".format(
@@ -155,22 +182,22 @@ class VideoLibrary:
                 )
             )
 
-        last_video_path = next(reversed(self.__playback_info))
+        last_video_path = next(reversed(self.__video_library))
 
-        for video_path, info in self.__playback_info.items():
-            if (info['next_frame'] < info['frame_count']
-                    and info['next_timestamp'] < info['duration']):
+        for video_path, video_info in self.__video_library.items():
+            if (video_info['next_frame'] < video_info['frame_count']
+                    and video_info['next_timestamp'] < video_info['duration']):
                 video = Video(video_path)
                 frame_rate = video.get_frame_rate()
 
                 if isinstance(skip, FrameSkip):
-                    frame = video.get_frame(info['next_frame'])
-                    info['next_frame'] += skip.amount
-                    info['next_timestamp'] = (info['next_frame'] / frame_rate) * 1000.0
+                    frame = video.get_frame(video_info['next_frame'])
+                    video_info['next_frame'] += skip.amount
+                    video_info['next_timestamp'] = (video_info['next_frame'] / frame_rate) * 1000.0
                 else:
-                    frame = video.get_frame(info['next_timestamp'])
-                    info['next_timestamp'] += skip.amount
-                    info['next_frame'] = int((info['next_timestamp'] / 1000.0) * frame_rate)
+                    frame = video.get_frame(video_info['next_timestamp'])
+                    video_info['next_timestamp'] += skip.amount
+                    video_info['next_frame'] = int((video_info['next_timestamp'] / 1000.0) * frame_rate)
 
                 self.__save_to_file()
 
