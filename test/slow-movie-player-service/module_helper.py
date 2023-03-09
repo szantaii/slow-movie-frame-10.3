7877cbd76c4b140a0f177bc0c9977021a0236c0a
szantaii <szantaii@gmail.com>
2023-03-09 18:10:29 +0100

    test/slow-movie-player-service/module_helper: Check existence of optional values

--- test/slow-movie-player-service/module_helper.py
+++ test/slow-movie-player-service/module_helper.py
@@ -10,11 +10,19 @@ def get_module_from_file(file_path: str) -> ModuleType:
     if module_name in sys.modules:
         return sys.modules[module_name]
 
-    if os.path.isabs(file_path):
-        spec = importlib.util.spec_from_file_location(module_name, file_path)
-    else:
-        current_directory = os.path.dirname(os.path.realpath(__file__))
-        spec = importlib.util.spec_from_file_location(module_name, os.path.join(current_directory, file_path))
+    if not os.path.isabs(file_path):
+        file_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            file_path
+        )
+
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+
+    if spec is None:
+        raise RuntimeError("Cannot get ModuleSpec from path '{}'!".format(file_path))
+
+    if spec.loader is None:
+        raise RuntimeError("ModuleSpec ('{}') has no loader!".format(spec.name))
 
     module = importlib.util.module_from_spec(spec)
 
